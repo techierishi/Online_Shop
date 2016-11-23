4c69056f01ea81b1c166df2ca0035d5ef9ed9971
techierishi <techierishi@gmail.com>
2016-11-23 12:00:47 +0530

    Category servlet

---
+++ src/org/ekart/admin/servlet/AddCategoryServlet.java
@@ -0,0 +1,44 @@
+package org.ekart.admin.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddCategoryServlet
+ */
+@WebServlet("/admin/category/*")
+public class AddCategoryServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddCategoryServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String category_name = request.getParameter("category_name");
+		String category_detail = request.getParameter("category_detail");
+		response.getWriter().append(" category_name").append(category_name).append(" category_detail ").append(category_detail);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
